15064f586d3c2fd4d4c372e50e3aa62243df1c76
Nicholas Contreras <nicholasc2000@yahoo.com>
2020-12-19 08:58:39 -0800

    init commit

---
+++ server.py
@@ -0,0 +1,43 @@
+# import socket
+
+# server_socket = socket.create_server(('', 80))
+# server_socket.listen()
+
+# minecraft_connection = None
+
+# while True:
+
+#     print('waiting for connection')
+#     socket, address = server_socket.accept()
+#     print('connection received')
+#     socket.sendall(b'hello')
+#     socket.close()
+
+    # if not minecraft_connection:
+    #     minecraft_connection = socket
+    #     print('Minecraft connection established')
+    # else:
+    #     while True:
+    #         data = socket.recv(1024)
+    #         if data:
+    #             minecraft_connection.sendall(data)
+    #         else: 
+    #             break
+
+    #     while True:
+    #         data = minecraft_connection.recv(1024)
+    #         if data:
+    #             socket.sendall(data)
+    #         else: 
+    #            break
+
+    #     socket.close()
+
+# Method called to handle web requests
+def run_server(environ, start_response):
+
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len("Hello World!")))
+    ])
+    return "Hello World!"
