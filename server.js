b4b193db660aab0e0d68299011cd76d4db867365
Nicholas Contreras <nicholasc2000@yahoo.com>
2022-07-10 18:55:43 -0500

    first websocket attempt

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
-const http = require('http');
+import { createServer } from "net";
+import { WebSocketServer } from "ws";
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+let computercraftSocket = false;
+let pendingResponses = {};
+
+const netServer = createServer((socket) => {
+
+    if (!computercraftSocket) {
+        socket.destroy();
+        return;
+    }
+
+    socket.setEncoding("utf8")
+    socket.on("data", (data) => {
+        const requestID = Date.now() + Math.random();
+        pendingResponses[requestID] = socket;
+        const requestText = requestID + "\r\n" + data.toString();
+
+        console.log(requestText);
+
+        computercraftSocket.send(requestText);
+    });
 });
 
-server.listen(port, () => {
+netServer.listen(port, () => {
     console.log(`Server running on port ${port}`);
+});
+
+const wsServer = new WebSocketServer({ server: netServer });
+wsServer.on("connection", (ws) => {
+    console.log("Websocket connection!");
+
+    if (!computercraftSocket) {
+        computercraftSocket = ws;
+        computercraftSocket.on("message", (data) => {
+            const requestID = data.substring(0, data.indexOf("\r\n"));
+            const requestText = data.substring(data.indexOf("\r\n") + 2);
+
+            pendingResponses[requestID].end(requestText);
+            pendingResponses.delete(requestID);
+        });
+        computercraftSocket.on("close", () => {
+            computercraftSocket = false;
+        })
+    }
 });
